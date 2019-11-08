20ba02ff5a8c1e3aa78d1a231744e726f555111d
dineshkumarr <dineshkumarr@DPSCHOIDT073.deanta.net>
2019-11-08 21:02:36 +0530

    Source driver

---
+++ src/main/java/com/LanstadV2/utilities/DriverSuppliers.java
@@ -0,0 +1,20 @@
+package com.LanstadV2.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverSuppliers {
+public WebDriver getdriver()
+{
+	WebDriver driver=null;
+	String path=null;
+	String browser=null;
+	if(browser=="chrome")
+	{
+		path=System.getProperty("user.dir");
+		
+		
+	}
+	
+	return null;
+}
+}
